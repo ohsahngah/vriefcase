cb25d0345f2f42cfbce2901453827d97a9a3f2d4
ohsahngah <ohsahngah@gmail.com>
2026-08-25 12:40:05 +0900

    Add files via upload

---
+++ assets/scripts/main.js
@@ -0,0 +1,7 @@
+"use strict";
+
+document.addEventListener("DOMContentLoaded", () => {
+    // DOM이 준비된 후 실행
+    const title = document.querySelector("h1");
+    console.log(title.textContent);
+});
